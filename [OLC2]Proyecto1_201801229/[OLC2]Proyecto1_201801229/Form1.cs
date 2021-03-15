177e6810c9304d9d9333fd223f839f5dca7fdc70
Osman Pérez <2999565980101@ingenieria.usac.edu.gt>
2021-03-14 18:23:00 -0600

    Cargar Archivo
    cargar un archivo

--- [OLC2]Proyecto1_201801229/[OLC2]Proyecto1_201801229/Form1.cs
+++ [OLC2]Proyecto1_201801229/[OLC2]Proyecto1_201801229/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -38,7 +40,20 @@ namespace _OLC2_Proyecto1_201801229
 
         private void Cargar_Click(object sender, EventArgs e)
         {
-
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var sr = new StreamReader(openFileDialog1.FileName);
+                    Codigo.Text=sr.ReadToEnd();
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
         }
 
         private void Ejecutar_Click(object sender, EventArgs e)
